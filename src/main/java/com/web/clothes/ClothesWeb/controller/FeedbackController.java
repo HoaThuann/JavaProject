96f3120ab9c4ff9f805b31f94f15c6ede3c84434
P-M-Trung <101286359+P-M-Trung@users.noreply.github.com>
2023-05-14 11:27:18 +0700

    no message

--- src/main/java/com/web/clothes/ClothesWeb/controller/FeedbackController.java
+++ src/main/java/com/web/clothes/ClothesWeb/controller/FeedbackController.java
@@ -2,6 +2,7 @@ package com.web.clothes.ClothesWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.web.clothes.ClothesWeb.service.FeedbackService;
 
@@ -9,4 +10,14 @@ import com.web.clothes.ClothesWeb.service.FeedbackService;
 public class FeedbackController {
 	@Autowired
 	private FeedbackService feedbackService;
+	
+	@RequestMapping("feedback")
+	public String feedback() {
+		return"admin/feedback";
+	}
+	
+	@RequestMapping("feedbackDetail")
+	public String feedbackView() {
+		return"admin/feedbackDetail";
+	}
 }
