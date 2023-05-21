ce88c5b50f8e30e5ad9486df4e0c280c31069e92
P-M-Trung <101286359+P-M-Trung@users.noreply.github.com>
2023-05-21 11:25:41 +0700

    no message

--- src/main/java/com/web/clothes/ClothesWeb/controller/FeedbackController.java
+++ src/main/java/com/web/clothes/ClothesWeb/controller/FeedbackController.java
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.web.clothes.ClothesWeb.dto.responseDto.CategoryPageResponseDto;
-import com.web.clothes.ClothesWeb.dto.responseDto.CategoryResponseDto;
 import com.web.clothes.ClothesWeb.dto.responseDto.FeedbackPageResponseDto;
 import com.web.clothes.ClothesWeb.dto.responseDto.FeedbackResponseDto;
-import com.web.clothes.ClothesWeb.entity.Category;
 import com.web.clothes.ClothesWeb.entity.Feedback;
 import com.web.clothes.ClothesWeb.service.FeedbackService;
 
@@ -25,31 +22,30 @@ import com.web.clothes.ClothesWeb.service.FeedbackService;
 public class FeedbackController {
 	@Autowired
 	private FeedbackService feedbackService;
-	
+
 	@RequestMapping("feedback")
 	public String feedbackView() {
-		return"admin/feedback";
+		return "admin/feedback";
 	}
-	
-	//get page category
-		@GetMapping(value = "/getCategoryPage")
-		@ResponseBody
-		public ResponseEntity<FeedbackPageResponseDto> getFeedbackPage(
-				@RequestParam(defaultValue = "8") int size, @RequestParam(defaultValue = "0") int page) {
 
-			Page<Feedback> feedbackPage = feedbackService.getAllFeedback(page, size);
-			
-			List<FeedbackResponseDto> feedbackResponseDtos = feedbackPage.stream()
-					.map(category -> new CategoryResponseDto(category.getId(),
-							category.getCategoryName()))
-					.collect(Collectors.toList());
-			
-			CategoryPageResponseDto categoryPageResponseDto = new CategoryPageResponseDto(
-					categoryPage.getTotalPages(), categoryPage.getNumber(), categoryPage.getSize(),
-					categoryResponseDtos);
-			
-			return ResponseEntity.ok(categoryPageResponseDto);
+	// get page category
+	@GetMapping(value = "/getFeedbackPage")
+	@ResponseBody
+	public ResponseEntity<FeedbackPageResponseDto> getFeedbackPage(@RequestParam(defaultValue = "8") int size,
+			@RequestParam(defaultValue = "0") int page) {
 
-		}
-	
+		Page<Feedback> feedbackPage = feedbackService.getAllFeedback(page, size);
+
+		List<FeedbackResponseDto> feedbackResponseDtos = feedbackPage.stream()
+				.map(feedback -> new FeedbackResponseDto(feedback.getEmail(), feedback.getFullName(),
+						feedback.getPhone(), feedback.getSubjectName()))
+				.collect(Collectors.toList());
+
+		FeedbackPageResponseDto feedbackPageResponseDto = new FeedbackPageResponseDto(feedbackPage.getTotalPages(),
+				feedbackPage.getNumber(), feedbackPage.getSize(), feedbackResponseDtos);
+
+		return ResponseEntity.ok(feedbackPageResponseDto);
+
+	}
+
 }
